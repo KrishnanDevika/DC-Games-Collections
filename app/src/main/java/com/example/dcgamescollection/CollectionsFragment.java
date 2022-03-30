97dbf7c6d5131c83b113a884fbb814bc3c0891c9
Chintan Ramparia <w0771148@myscc.ca>
2022-03-30 10:47:15 -0400

    #23, #24 Created Collection View Adapter

--- app/src/main/java/com/example/dcgamescollection/CollectionsFragment.java
+++ app/src/main/java/com/example/dcgamescollection/CollectionsFragment.java
@@ -3,6 +3,7 @@ package com.example.dcgamescollection;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
@@ -55,10 +56,22 @@ public class CollectionsFragment extends Fragment {
         }
     }
 
+    /**
+     *
+     * @Author: Chintan Ramparia
+     * @Date: March 30th 2022
+     *
+     * @Content: Linking Adapter & Holders to Fragments.
+     */
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_collections, container, false);
+        View view = inflater.inflate(R.layout.fragment_collections, container, false);
+
+        RecyclerView recyclerView = view.findViewById(R.id.collectionList);
+
+
+        return view;
     }
 }
