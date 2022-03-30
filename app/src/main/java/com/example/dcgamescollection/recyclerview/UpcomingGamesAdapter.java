e27c18d0d00372c8faa229f166594e21596dc0a8
Alex Coutinho <W0452854@myscc.ca>
2022-03-30 10:24:32 -0400

    #19: Created Recycler Adapter for Upcoming Games lists. * Added CustomHolder class to Adapter. Grabs all relevant objects from UI layout. * Cannot do more without pojo object and DatabaseHelper.

---
+++ app/src/main/java/com/example/dcgamescollection/recyclerview/UpcomingGamesAdapter.java
@@ -0,0 +1,54 @@
+package com.example.dcgamescollection.recyclerview;
+
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+import androidx.viewpager2.widget.ViewPager2;
+
+import com.example.dcgamescollection.R;
+
+public class UpcomingGamesAdapter extends RecyclerView.Adapter<UpcomingGamesAdapter.CustomHolder>{
+
+
+    @NonNull
+    @Override
+    public UpcomingGamesAdapter.CustomHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull UpcomingGamesAdapter.CustomHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+
+    class CustomHolder extends RecyclerView.ViewHolder {
+
+        protected ViewPager2 imageViewPager;
+        protected TextView gameName;
+        protected TextView releaseDate;
+        protected TextView rating;
+        protected Button moreButton;
+        protected Button saveButton;
+
+
+        public CustomHolder(@NonNull View itemView) {
+            super(itemView);
+            this.imageViewPager = itemView.findViewById(R.id.viewPager2);
+            this.gameName = itemView.findViewById(R.id.textView2);
+            this.releaseDate = itemView.findViewById(R.id.textView5);
+            this.rating = itemView.findViewById(R.id.textView4);
+            this.moreButton = itemView.findViewById(R.id.button2);
+            this.saveButton = itemView.findViewById(R.id.button3);
+        }
+    }
+
+}
