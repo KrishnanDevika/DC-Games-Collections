2f9abb5fd1083738c42adce0916bd00bc4d68c84
Alex Coutinho <W0452854@myscc.ca>
2022-04-06 09:43:32 -0400

    #37: Created base layout and class for Trending Games page * Need to find out how to search by rating.

---
+++ app/src/main/java/com/example/dcgamescollection/TrendingGames.java
@@ -0,0 +1,132 @@
+package com.example.dcgamescollection;
+
+import android.os.Bundle;
+import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.example.dcgamescollection.Api.GameSingleton;
+import com.example.dcgamescollection.Pojo.Games;
+import com.example.dcgamescollection.SearchRecyclerView.CustomSearchAdapterView;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Calendar;
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Use the {@link TrendingGames#newInstance} factory method to
+ * create an instance of this fragment.
+ *
+ */
+public class TrendingGames extends Fragment {
+
+    // TODO: Rename parameter arguments, choose names that match
+    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+    public static final String ADD_TO_COLLECTION = "save";
+    public static final String ACTION_TYPE = "action_type";
+    private static final String ARG_PARAM1 = "param1";
+    private static final String ARG_PARAM2 = "param2";
+    private RecyclerView gameRecyclerView;
+    private CustomSearchAdapterView adapterView;
+    private ArrayList<Games> gamesList;
+
+    // TODO: Rename and change types of parameters
+    private String mParam1;
+    private String mParam2;
+
+    /**
+     * Use this factory method to create a new instance of
+     * this fragment using the provided parameters.
+     *
+     * @param param1 Parameter 1.
+     * @param param2 Parameter 2.
+     * @return A new instance of fragment TrendingGames.
+     */
+    // TODO: Rename and change types and number of parameters
+    public static TrendingGames newInstance(String param1, String param2) {
+        TrendingGames fragment = new TrendingGames();
+        Bundle args = new Bundle();
+        args.putString(ARG_PARAM1, param1);
+        args.putString(ARG_PARAM2, param2);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    public TrendingGames() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            mParam1 = getArguments().getString(ARG_PARAM1);
+            mParam2 = getArguments().getString(ARG_PARAM2);
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.fragment_trending_games, container, false);
+        gameRecyclerView = view.findViewById(R.id.trendingRecycler);
+        gamesList = new ArrayList<>();
+        getData();
+        return view;
+    }
+
+    private void getData(){
+        Games game = new Games();
+        String API_KEY = "1e670774e0cc43a993b99c3b3a56913e";
+
+        String url = ("https://api.rawg.io/api/games?key="+API_KEY+"&rating=4.5,5.0");
+
+        Log.d("ResultSearch", url);
+        JsonObjectRequest objectRequest = new JsonObjectRequest(Request.Method.GET, url, null, new Response.Listener<JSONObject>() {
+            @Override
+            public void onResponse(JSONObject response) {
+                try {
+                    JSONArray jsonArray = response.getJSONArray("results");
+                    for (int i = 0; i < jsonArray.length(); i++) {
+                        JSONObject results = jsonArray.getJSONObject(i);
+                        game.setName(results.getString("name"));
+                        game.setRating(results.getDouble("rating"));
+                        game.setReleaseDate(results.getString("released"));
+                        game.setGameIcon(results.getString("background_image"));
+                        gamesList.add(new Games(results.getString("name"), results.getString("released"),results.getString("background_image"), results.getDouble("rating")));
+                        Bundle extra = new Bundle();
+                        extra.putString(ACTION_TYPE, ADD_TO_COLLECTION);
+                        adapterView = new CustomSearchAdapterView(gamesList, getContext(), extra);
+                        gameRecyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
+                        gameRecyclerView.setAdapter(adapterView);
+                    }
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Log.d("VOLLEY_ERROR", error.getLocalizedMessage());
+            }
+        });
+
+        GameSingleton.getInstance(getContext()).getRequestQueue().add(objectRequest);
+    }
+}
